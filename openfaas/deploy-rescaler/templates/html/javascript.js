f51d2d343b9cef51c584ad680ea3720eb17f7777
fjudith <florian.judith.b@gmail.com>
2020-02-18 00:57:00 +0100

    OpenFaaS: Enabled responsive dark-mode

---
+++ openfaas/deploy-rescaler/templates/html/javascript.js
@@ -0,0 +1,46 @@
+"use strict";
+window.addEventListener("load", function () {
+    function sendData() {
+        var path = "/function/rescale-replicas.openfaas-fn"
+
+        const XHR = new XMLHttpRequest();
+
+        // Bind the FormData object and the form element
+        const FD = new FormData(form);
+
+        // Define what happens on successful data submission
+        XHR.addEventListener("load", function (event) {
+            document.getElementById('output').innerHTML = event.target.responseText;
+            console.info(event.target.responseText);
+        });
+
+        // Define what happens in case of error
+        XHR.addEventListener("error", function (event) {
+            alert("Oops! Something went wrong.");
+        });
+
+        // Set up our request
+        XHR.open("POST", path);
+
+        XHR.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        var json = {};
+        for (const [key, value] of FD.entries()) {
+            json[key] = value;
+        }
+        var json = JSON.stringify(json);
+        console.info(json);
+
+        // The data sent is what the user provided in the form
+        XHR.send(json);
+    }
+
+    // Access the form element...
+    var form = document.getElementById("rescaler");
+
+    // ...and take over its submit event.
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        sendData();
+    });
+});
